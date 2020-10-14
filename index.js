12f28b92257c83cc6ecf578a085b38f1bbe747f5
Shawn Harrington <shawn.harrington2776@gmail.com>
2020-10-14 04:37:16 -0700

    finished task 5git add .

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ function getYears(getFinals) {
     return years;
 };
 
+getYears(getFinals);
+
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
 function getWinners(getFinals) {
@@ -53,7 +55,9 @@ function getWinners(getFinals) {
     return finalWinners
 };
 
-getWinners(getFinals);
+const ohWell = getWinners(getFinals);
+
+console.log(ohWell);
 
 
 /* Task 5: Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!" 
@@ -63,11 +67,17 @@ Parameters:
  * callback function getYears
  */
 
-function getWinnersByYear(/* code here */) {
+function getWinnersByYear(getWinners, getYears) {
+    const finalWinners = getWinners(getFinals);
+    const finalYears = getYears(getFinals);
 
+    finalWinners.forEach((winner, index) => {
+        const year = finalYears[index];
+        console.log(`In ${year}, ${winner} won the world cup!`);
+    });
 };
 
-getWinnersByYear();
+getWinnersByYear(getWinners, getYears);
 
 /* Task 6: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
